c352dcfd4418f0eda2bc5d02e3c01bf4e15063d5
かずま <51999502+kazumax7@users.noreply.github.com>
2020-06-22 23:54:37 +0900

    a

--- script/share.js
+++ script/share.js
@@ -9,14 +9,24 @@ async function share(title, text, url){
 }
 
 function shareOnTwitter(text = "", url = "", hashtags = "", via = "", related = "", in_reply_to = ""){
-    let data = [
-        `text=${text}`,
-        `url=${url}`,
-        `hashtags=${hashtags}`,
-        `via=${via}`,
-        `related=${related}`,
-        `in_reply_to=${in_reply_to}`,
-    ].join("&");
+    let data = [];
+
+    if(text.length > 0){
+        data.push(`text=${text}`);
+    }else if(url.length > 0){
+        data.push(`url=${url}`);
+    }else if(hashtags.length > 0){
+        data.push(`hashtags=${hashtags}`);
+    }else if(via.length > 0){
+        data.push(`via=${via}`);
+    }else if(related.length > 0){
+        data.push(`related=${related}`);
+    }else if(in_reply_to.length > 0){
+        data.push(`in_reply_to=${in_reply_to}`);
+    }
+
+    data = data.join("&");
+
     window.open(`https://twitter.com/intent/tweet?${data}`);
 }
 
