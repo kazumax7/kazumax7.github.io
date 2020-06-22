b7fe90ed042f5ec7f78dc3dc89882ee0f4d112d8
かずま <51999502+kazumax7@users.noreply.github.com>
2020-06-22 20:59:02 +0900

    a

--- script/index.js
+++ script/index.js
@@ -1,13 +1,33 @@
-function share(){
-    if (navigator.share) {
+async function share(title, text, url){
+    if(navigator.share){
         navigator.share({
-            title: 'MDN',
-            text: 'Learn web development on MDN!',
-            url: 'https://developer.mozilla.org',
+            title: title,
+            text: text,
+            url: url
         });
     }
-    //サポートしていない場合の処理
-    else {
-        alert('このブラウザはシェア機能に対応していません。');
-    }
+}
+
+function shareOnTwitter(text = "", url = "", hashtags = "", via = "", related = "", in_reply_to = ""){
+    let data = [
+        `text=${text}`,
+        `url=${url}`,
+        `hashtags=${hashtags}`,
+        `via=${via}`,
+        `related=${related}`,
+        `in_reply_to=${in_reply_to}`,
+    ].join("&");
+    window.open(`https://twitter.com/intent/tweet?${data}`);
+}
+
+function shareUrlOnFacebook(url){
+    window.open(`https://www.facebook.com/sharer/sharer.php?u=${url}`);
+}
+
+function shareUrlOnLINE(url){
+    window.open(`https://social-plugins.line.me/lineit/share?url=${url}`);
+}
+
+function shareOnLINE(text){
+    window.open(`http://line.me/R/msg/text/?${text}`);
 }
